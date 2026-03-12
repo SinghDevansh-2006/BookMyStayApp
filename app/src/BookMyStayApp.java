fb8d85d8e99f29ba1c5d164136d9da166d5eaaf3
SinghDevansh-2006 <devansh.singh24102006@gmail.com>
2026-03-12 10:00:11 +0530

    Fixed UC9 validation implementation

--- app/src/BookMyStayApp.java
+++ app/src/BookMyStayApp.java
@@ -14,6 +14,7 @@ class RoomInventory {
     public int getAvailability(String roomType) {
         return roomAvailability.getOrDefault(roomType, 0);
     }
+
 }
 
 class InvalidBookingException extends Exception {
